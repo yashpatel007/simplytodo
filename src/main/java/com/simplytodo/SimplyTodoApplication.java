1e22df9f843159c1bf7d77dc71d72f4263901994
Yash Patel <patelyash1311@gmail.com>
2023-10-31 19:12:47 -0400

    project setup

---
+++ src/main/java/com/simplytodo/SimplyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.simplytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplytodo/SimplyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
